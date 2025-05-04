afd7c4b3e401545458039758d99d9054be4f1ce6
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:48:55 +0300

    completed 6 exercise

--- solutions/6-timers.js
+++ solutions/6-timers.js
@@ -1,5 +1,25 @@
 import fs from 'fs';
 
 // BEGIN
+const watch = (filepath, time, cb) => {
+    let lastCheckTime = Date.now();
 
+    const timerId = setInterval(() => {
+        fs.stat(filepath, (_error, stats) => {
+            if (_error) {
+                clearInterval(timerId);
+                cb(_error);
+                return;
+            }
+            let currentMTime = stats.mtimeMs;
+            if (currentMTime > lastCheckTime) {
+                lastCheckTime = currentMTime;
+                cb(null);
+            }
+        });
+    }, time);
+    return timerId;
+}
+
+export default watch
 // END
