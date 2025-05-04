336d21c959118c9c568c5bbe8aaf1afe96cfd2e7
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:48:46 +0300

    completed 5 exercise

--- solutions/5-parallel-execution.js
+++ solutions/5-parallel-execution.js
@@ -4,5 +4,30 @@ import _ from 'lodash';
 import async from 'async';
 
 // BEGIN
-
+export const getDirectorySize = (dirPath, cb) => {
+    fs.readdir(dirPath, (_error1, files) => {
+        if (_error1) {
+            cb(_error1);
+            return;
+        };
+        const fullPath = files.map(file => path.join(dirPath, file));
+        async.map(fullPath, (filepath, done) => {
+            fs.stat(filepath, (_error2, stats) => {
+                if (_error2) {
+                    done(null, 0);
+                    return;
+                }
+                done(null, stats.isFile() ? stats.size : 0);
+            });
+        }, 
+        (_error3, results) => {
+            if (_error3) {
+                cb(_error3);
+                return;
+            }
+            const totalSize = _.sumBy(results);
+            cb(null, totalSize);
+        });
+    });
+}
 // END
