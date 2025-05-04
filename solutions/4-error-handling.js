f19f007b6749a4257f893cef5af7e7e5975dbd05
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:48:37 +0300

    completed 4 exercise

--- solutions/4-error-handling.js
+++ solutions/4-error-handling.js
@@ -1,5 +1,25 @@
 import fs from 'fs';
 
 // BEGIN
-
+export const move = (filepathFrom, filepathTo, cb) => {
+    fs.readFile(filepathFrom, 'utf-8', (_error1, data) => {
+        if (_error1){
+            cb(_error1);
+            return;
+        };
+        fs.writeFile(filepathTo, data, (_error2) => {
+            if(_error2){
+                cb(_error2);
+                return;
+            };
+            fs.unlink(filepathFrom, (_error3) => {
+                if(_error3){
+                    cb(_error3);
+                    return;
+                };
+                cb(null)
+            });
+        });
+    });
+}
 // END
