271e47acbc865023c37a17aa0e55697e289222fd
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:48:21 +0300

    completed 2 exercise

--- solutions/2-asynchronous-return.js
+++ solutions/2-asynchronous-return.js
@@ -1,5 +1,12 @@
 import fs from 'fs';
 
 // BEGIN
+const write = (filepath, data, callback) => {
+    if (data === ''){
+        return;
+    }
+    fs.writeFile(filepath, data, callback);
+}
 
+export default write;
 // END
