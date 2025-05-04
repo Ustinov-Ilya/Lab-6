5c2ef45f8529e9f9796b0c032cfa5b2d53d3603a
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:49:03 +0300

    completed 7 exercise

--- solutions/7-promises.js
+++ solutions/7-promises.js
@@ -1,5 +1,9 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const reverse = (file) => {
+    return fsp.readFile(file, 'utf-8')
+        .then((lines) => lines.split('\n').reverse().join('\n'))
+        .then((lines) => fsp.writeFile(file, lines))
+}
 // END
