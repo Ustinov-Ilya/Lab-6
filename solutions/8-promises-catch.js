08ebddd3988702250c64802d65ab6f46372e02f1
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:49:10 +0300

    completed 8 exercise

--- solutions/8-promises-catch.js
+++ solutions/8-promises-catch.js
@@ -1,5 +1,11 @@
 import fsp from 'fs/promises';
 
 // BEGIN
+export const touch = (filepath) => {
+    return fsp.access(filepath, fsp.constants.R_OK | fsp.constants.W_OK)
+        .catch(() => {
+            fsp.writeFile(filepath, '')
+        })
+}
 
 // END
