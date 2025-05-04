443d8d227bf971f186c9fc9e861eba62d9081e8a
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:49:16 +0300

    completed 9 exercise

--- solutions/9-chain-of-promises.js
+++ solutions/9-chain-of-promises.js
@@ -1,5 +1,12 @@
 import fsp from 'fs/promises';
+import path from 'path';
 
 // BEGIN
-
+export const getTypes = (listPaths) => {
+    return Promise.all(listPaths.map((path) => fsp.stat(path)
+            .then((types) => types.isDirectory() ? 'directory' : 'file')
+            .catch(() => null)
+        )
+    );
+}
 // END
