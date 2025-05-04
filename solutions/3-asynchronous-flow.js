13b6fcb73682bbefd48994cf50f0b0e670ebdd71
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-04 16:48:28 +0300

    completed 3 exercise

--- solutions/3-asynchronous-flow.js
+++ solutions/3-asynchronous-flow.js
@@ -1,5 +1,13 @@
 import fs from 'fs';
 
 // BEGIN
-
+export const compareFileSizes = (filepath1, filepath2, callback) => {
+    fs.stat(filepath1, (_error1, stats1) => {
+        let size1 = stats1.size;
+        fs.stat(filepath2, (_error2, stats2) => {
+            let size2 = stats2.size;
+            callback(null, Math.sign(size1-size2));
+        });
+    });
+}
 // END
